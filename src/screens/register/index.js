04f965097cd3cca4a2ff6347e3d80bd3db6f1ff6
RaCaTaSh <racatashsp@gmail.com>
2021-08-13 12:40:34 -0300

    Validaciones y mostrar errores

--- src/screens/register/index.js
+++ src/screens/register/index.js
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 import { Formik } from "formik";
 import * as Yup from "yup";
+import "./styles.css";
 
+//Esquema de Yup
 const RegisterSchema = Yup.object().shape({
-  name: Yup.string().required("Este campo es obligatorio"),
+  name: Yup.string()
+    .required("Este campo es obligatorio")
+    .matches(/^[a-zA-Z\s]*$/, "Solo ingresar texto"),
   id: Yup.number()
     .required("Este campo es obligatorio")
     .min(10000000, "El dni debe ser mayor a 10 millones")
@@ -16,76 +20,135 @@ const RegisterSchema = Yup.object().shape({
     .required("Este campo es obligatorio")
     .min(8, "Debe tener al menos 8 caracteres")
     .matches(/(?=\w*[A-Z])/, "Debe contener al menos una mayuscula"),
+  repeatPassword: Yup.string()
+    .required("Este campo es obligatorio")
+    .oneOf([Yup.ref("password")], "Las contraseñas no coinciden"),
 });
+
 const Register = () => {
   const [visible, setVisible] = useState(false);
-  let text = "Mostrar Contraseña";
   return (
-    <div className="container  p-3">
+    <div className="container px-5 py-5">
+      {/* Manera que se rellenan los campos que creamos y que empiezan estando vacios y se llenan con el fomulario */}
       <Formik
-        initialValues={{ name: "", id: "", phone: "", email: "", password: "" }}
+        initialValues={{
+          name: "",
+          id: "",
+          phone: "",
+          email: "",
+          password: "",
+          repeatPassword: "",
+        }}
+        //Validacion
         validationSchema={RegisterSchema}
+        //Forma de mandar todo el formulario y obtener los datos
+        onSubmit={((values)=>{console.log(values);})}
       >
-        {({ handleBlur, handleChange, handleSubmit, values, errors }) => (
-          <>
+        {/* Momento en el que se muestren los mensajes de error de las validaciones que hicimos */}
+        {({
+          handleBlur,
+          handleChange,
+          handleSubmit,
+          values,
+          errors,
+          touched,
+        }) => (
+          <form onSubmit={handleSubmit}>
             <input
               placeholder="Nombre completo"
               name="name"
               onChange={handleChange("name")}
+              /* onBlur={handleBlur("name")} */
               value={values.name}
               className="form-control"
             />
-            {errors.name && <p>{errors.name}</p>}
+            {touched.name && errors.name && (
+              <p className="error">{errors.name}</p>
+            )}
             <br />
             <input
               placeholder="Numero de documento"
               name="id"
               type="number"
               onChange={handleChange("id")}
+              /* onBlur={handleBlur("id")} */
               value={values.id}
               className="form-control"
             />
-            {errors.id && <p>{errors.id}</p>}
+            {touched.id && errors.id && <p className="error">{errors.id}</p>}
             <br />
             <input
               placeholder="Numero de telefono"
               name="phone"
               type="number"
               onChange={handleChange("phone")}
+              /* onBlur={handleBlur("phone")} */
               value={values.phone}
               className="form-control"
             />
-            {errors.phone && <p>{errors.phone}</p>}
+            {touched.phone && errors.phone && (
+              <p className="error">{errors.phone}</p>
+            )}
             <br />
             <input
               placeholder="Correo electronico"
               name="email"
               onChange={handleChange("email")}
+              /* onBlur={handleBlur("email")} */
               value={values.email}
               className="form-control"
             />
-            {errors.email && <p>{errors.email}</p>}
+            {touched.email && errors.email && (
+              <p className="error">{errors.email}</p>
+            )}
             <br />
             <div className="input-group-append">
+              <div className="passWithButton">
+                <input
+                  placeholder="Contraseña"
+                  name="password"
+                  onChange={handleChange("password")}
+                  /* onBlur={handleBlur("password")} */
+                  value={values.password}
+                  type={visible ? "text" : "password"}
+                  className="form-control"
+                />
+                <button
+                  className="btn btn-secondary passButton"
+                  onClick={() => setVisible(!visible)}
+                >
+                  {visible ? <p>OCultar</p> : <p>Mostrar</p>}
+                </button>
+              </div>
+              {touched.password && errors.password && (
+                <p className="error">{errors.password}</p>
+              )}
+              <br />
+              <div className="passWithButton">
+                <input
+                  placeholder="Repetir contraseña"
+                  name="repeatPassword"
+                  onChange={handleChange("repeatPassword")}
+                  /* onBlur={handleBlur("repeatPassword")} */
+                  value={values.repeatPassword}
+                  type={visible ? "text" : "password"}
+                  className="form-control"
+                />
 
-         
-            <input
-              placeholder="Contraseña"
-              name="password"
-              onChange={handleChange("password")}
-              value={values.password}
-              type={visible ? "text" : "password"}
-              className="form-control"
-            />
-           
-            <button className="btn btn-secondary " onClick={() => setVisible()}>
-              {text}
-            </button>
+                <button
+                  className="btn btn-secondary passButton"
+                  onClick={() => setVisible(!visible)}
+                >
+                  {visible ? <p>OCultar</p> : <p>Mostrar</p>}
+                </button>
+              </div>
+              {touched.repeatPassword && errors.repeatPassword && (
+                <p className="error">{errors.repeatPassword}</p>
+              )}
             </div>
-            {errors.password && <p>{errors.password}</p>}
             <br />
-            <button className="btn btn-primary">Registrarme</button>
-          </>
+            <button type="submit" className="btn btn-primary">Registrarme</button>
+          </form>
         )}
       </Formik>
     </div>
